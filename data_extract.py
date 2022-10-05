dbd22cb848377c662d72929407e18d0f10509b4f
gbournigal <georgesbournigal@gmail.com>
2022-10-05 14:42:51 -0400

    Primera subida para probar
    Probar lo de online learning y como mejora con esto

--- data_extract.py
+++ data_extract.py
@@ -9,10 +9,11 @@ import pandas as pd
 import numpy as np
 import itertools
 import gc  # garbage collection
+from lightgbm import LGBMClassifier
 
 # data loading
 DEBUG = False
-SAMPLE = 0.01
+SAMPLE = 0.5
 SEED = 42
 
 col_dtypes = {
@@ -42,7 +43,7 @@ cols = list(col_dtypes.keys())
 
 path_to_data = 'data'
 df = pd.DataFrame({}, columns=cols)
-for i in range(2):
+for i in range(10):
     df_tmp = pd.read_csv(f'{path_to_data}/train_{i}.csv', dtype=col_dtypes)
     if SAMPLE < 1:
         df_tmp = df_tmp.sample(frac=SAMPLE, random_state=SEED)
@@ -66,3 +67,121 @@ pos_groups = {
 
 for col, vec in pos_groups.items():
     df[col + "_ball_dist"] = euclidian_norm(df[vec].values - df[pos_groups["ball_pos"]].values)
+
+import math
+def calculate_distance_1(x1,y1,z1,x2,y2,z2):
+    d = 0.0
+    d+= (x1-x2)**2
+    d+= (y1-y2)**2
+    d+= (z1-z2)**2
+    return math.sqrt(d)
+
+    
+for p in ['p0','p1','p2','p3','p4','p5']:
+    col1 = p+'_dist_B_goal'
+    col2 = p+'_dist_A_goal'
+    p_x = p+'_pos_x'
+    p_y = p+'_pos_y'
+    p_z = p+'_pos_z'
+    df[col1] = df.apply(lambda x: calculate_distance_1(x[p_x], x[p_y], x[p_z], 0, -100, 6.8), axis=1)
+    df[col2] = df.apply(lambda x: calculate_distance_1(x[p_x], x[p_y], x[p_z], 0, 100, 6.8), axis=1)
+    
+    
+cols_to_drop = [
+    'game_num', 'event_id', 'event_time', 'player_scoring_next', 'team_scoring_next'
+]
+
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_validate
+
+df = df.drop(columns=cols_to_drop)
+df = df.dropna(axis=0)
+gc.collect()
+
+model_a = LGBMClassifier(n_jobs=-1)
+
+cv_a = cross_validate(
+    model_a, 
+    X=df.drop(columns=['team_A_scoring_within_10sec', 'team_B_scoring_within_10sec']).values,
+    y=df['team_A_scoring_within_10sec'].values,
+    scoring="neg_log_loss",
+    cv=5,
+    verbose=2,
+    return_estimator=True,
+    n_jobs=-1
+)
+
+df_cv_a = pd.DataFrame(
+    {
+        "model": "Model A",
+        "fold": list(range(5)),
+        "test_log_loss": - cv_a["test_score"]
+    }
+)
+
+model_b = RandomForestClassifier(n_jobs=-1)
+
+cv_b = cross_validate(
+    model_b, 
+    X=df.drop(columns=['team_A_scoring_within_10sec', 'team_B_scoring_within_10sec']).values,
+    y=df['team_B_scoring_within_10sec'].values,
+    scoring="neg_log_loss",
+    cv=5,
+    verbose=2,
+    return_estimator=True,
+    n_jobs=-1
+)
+
+df_cv_b = pd.DataFrame(
+    {
+        "model": "Model B",
+        "fold": list(range(5)),
+        "test_log_loss": - cv_b["test_score"]
+    }
+)
+
+
+df_test = pd.read_csv('data/test.csv')
+for col, vec in pos_groups.items():
+    df_test[col + "_ball_dist"] = euclidian_norm(df_test[vec].values - df_test[pos_groups["ball_pos"]].values)
+
+for p in ['p0','p1','p2','p3','p4','p5']:
+    col1 = p+'_dist_B_goal'
+    col2 = p+'_dist_A_goal'
+    p_x = p+'_pos_x'
+    p_y = p+'_pos_y'
+    p_z = p+'_pos_z'
+    df_test[col1] = df_test.apply(lambda x: calculate_distance_1(x[p_x], x[p_y], x[p_z], 0, -100, 6.8), axis=1)
+    df_test[col2] = df_test.apply(lambda x: calculate_distance_1(x[p_x], x[p_y], x[p_z], 0, 100, 6.8), axis=1)
+    
+
+from sklearn.impute import SimpleImputer
+imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+pred_a = np.zeros(df_test.shape[0])
+for estimator in cv_a['estimator']:
+    pred_a_X = imp.fit_transform(df_test.drop(columns=['id']))
+    pred_a += estimator.predict_proba(pred_a_X)[:, 1]
+
+pred_a /= 5
+
+
+pred_b = np.zeros(df_test.shape[0])
+for estimator in cv_b['estimator']:
+    pred_b_X = imp.fit_transform(df_test.drop(columns=['id']))
+    pred_b += estimator.predict_proba(pred_b_X)[:, 1]
+
+pred_b /= 5
+
+
+df_submission = pd.DataFrame(
+    {
+        "id": df_test['id'],
+        "team_A_scoring_within_10sec": pred_a,
+        "team_B_scoring_within_10sec": pred_b
+    }
+)
+
+df_submission.to_csv('submission.csv', index=False)
+
